88a65f146587afe4f3f495722d95e574d332ff25
Alan S. Tennent <tennent4@gmail.com>
2015-06-03 17:24:14 -0700

    Camera shift acceleration

--- Assets/Scripts/Camera/CitrusCamera.cs
+++ Assets/Scripts/Camera/CitrusCamera.cs
@@ -5,6 +5,7 @@ public class CitrusCamera : MonoBehaviour
 
    float x;
    float y;
+   float shiftVelocity;
 
    private float FREE_ROTATE_SPEED = 8f;
    private float FREE_MOVEMENT_SPEED = 5f;
@@ -21,10 +22,17 @@ public class CitrusCamera : MonoBehaviour
          transform.RotateAround(transform.position, Vector3.up, x);
       }
 
+      if (Input.GetButton("Shift")) {
+         shiftVelocity = Mathf.Max(shiftVelocity + .025f, 1.5f);
+      }
+      else {
+         shiftVelocity = Mathf.Max(shiftVelocity/2, 1);
+      }
+
       //Movement
-      float forward = Input.GetAxis("Vertical") * FREE_MOVEMENT_SPEED;
-      float strafe = Input.GetAxis("Horizontal") * FREE_MOVEMENT_SPEED;
-      float strafeVertical = Input.GetAxis("StrafeVertical") * FREE_MOVEMENT_SPEED;
+      float forward = Input.GetAxis("Vertical") * FREE_MOVEMENT_SPEED * shiftVelocity;
+      float strafe = Input.GetAxis("Horizontal") * FREE_MOVEMENT_SPEED * shiftVelocity;
+      float strafeVertical = Input.GetAxis("StrafeVertical") * FREE_MOVEMENT_SPEED * shiftVelocity;
 
       transform.position += transform.forward * forward;
       transform.position += transform.right * strafe;
