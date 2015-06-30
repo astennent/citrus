f87390373b9b4612b295822c4273bd47e11e3cbd
Alan S. Tennent <tennent4@gmail.com>
2015-06-29 21:57:10 -0700

    File Explorer 1

---
+++ Assets/FileExplorer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class FileExplorer : MonoBehaviour {
+
+   public RectTransform rectTransform;
+
+   void Start () {
+
+   }
+
+   void Update () {
+
+   }
+
+   public void OnDragResizer(BaseEventData data) {
+      Vector2 delta = ((PointerEventData)data).delta;
+      Vector2 currentSize = rectTransform.sizeDelta;
+      rectTransform.sizeDelta = new Vector2(currentSize.x + delta.x, currentSize.y - delta.y);
+   }
+}
